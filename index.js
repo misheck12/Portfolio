b15d4fbfe40d12aec4d38422ca443b51df8adc6a
misheck12 <misheck1720@gmail.com>
2022-07-03 16:43:13 +0200

    update

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const width = window.innerWidth;
 const cards = [
   {
     key: 0,
-    name: 'Tonic',
-    option: ['CANOPY', 'Back End Dev', '2015'],
+    name: 'Afro-Concert',
+    option: ['Microverse', 'Front End Dev', '2022'],
     optionMobile: ['CANOPY', 'Back End Dev', '2015'],
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    featuredImage: './images/photo1dt.png',
-    desktopImage: 'images/photo1dt.png',
-    technologies: ['html', 'css', 'javascript', 'ruby'],
+    description: 'The project is about Afrotech 2022 global Summit. The website gives information about the event The about-page can be accessed from the homepage with the About link.',
+    featuredImage: './images/afrotech.png',
+    desktopImage: 'images/afrotech.png',
+    technologies: ['html', 'css', 'javascript'],
     linktoliveversion: 'https://misheck12.github.io/Portfolio/',
     linktosource: 'https://github.com/misheck12/Portfolio',
   },
@@ -115,8 +115,8 @@ const Openpopup = ({
 function showWorks() {
   const works = [
     {
-      title: 'Tonic',
-      options: ['CANOPY', 'Back End Dev', '2015'],
+      title: 'Afrotech',
+      options: ['Microverse', 'Front End Dev', '2022'],
       imgMobile: 'images/photo1.png',
       imgDesktop: 'images/photo1dt.png',
       description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
